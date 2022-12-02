d220f1c930cab239e70e27ca1196814108dca36d
songjy002 <113113802+songjy002@users.noreply.github.com>
2022-12-02 14:55:02 +0900

    Add files via upload

--- final_project/stage1.gd
+++ final_project/stage1.gd
@@ -1,20 +1,32 @@
 extends Node2D
 
 var isRotate = false
+var enableKey = true
 
 func _process(delta):
-	if Input.is_action_just_pressed("rotate_right"):
+
+	
+	if Input.is_action_just_pressed("rotate_right") and enableKey:
+		enableKey = false
 		isRotate = true
 		for i in range(18):
 			rotation_degrees += 5
 			yield(get_tree().create_timer(delta/2), "timeout")
 		yield(get_tree().create_timer(delta), "timeout")
 		isRotate = false
+		yield(get_tree().create_timer(1), "timeout")
+		enableKey = true
 
-	if Input.is_action_just_pressed("rotate_left"):
+
+	if Input.is_action_just_pressed("rotate_left") and enableKey:
+		enableKey = false
 		isRotate = true
 		for i in range(18):
 			rotation_degrees -= 5
 			yield(get_tree().create_timer(delta/2), "timeout")
 		yield(get_tree().create_timer(delta), "timeout")
 		isRotate = false
+		yield(get_tree().create_timer(1), "timeout")
+		enableKey = true
+
+
