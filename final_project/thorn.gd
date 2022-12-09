7f214f55e641759dac3abe0b0b7beaaa73bb2dde
kangtp <115209262+kangtp@users.noreply.github.com>
2022-12-10 00:16:45 +0900

    Create thorn.gd

--- final_project/thorn.gd
+++ final_project/thorn.gd
@@ -4,8 +4,5 @@ extends Area2D
 
 
 func _on_thorn_body_entered(body):
-	if(GameData.clear == -1):
-		if(GameData.effectSound == 1):
-			KilledSound.play()
-		body.queue_free()
-		get_tree().change_scene("res://stage1.tscn")
+	body.queue_free()
+	get_tree().change_scene("res://stage3.tscn")
