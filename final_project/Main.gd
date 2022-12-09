476dcb6fb050617e3d2d25fd31ca5a9a73e5fe2f
seorinn <80616701+seorinn@users.noreply.github.com>
2022-12-09 14:52:46 +0900

    Add files via upload

--- final_project/Main.gd
+++ final_project/Main.gd
@@ -1,20 +1,6 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+var musicPlaying = 1
 
 func _on_CreditButton_pressed():
 	get_tree().change_scene("res://Credit.tscn")
@@ -26,3 +12,7 @@ func _on_RecordsButton_pressed():
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Stages.tscn")
+
+
+func _on_TextureButton_pressed():
+	musicPlaying *= -1
