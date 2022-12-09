a0fa06162428c939d47dd639e4b1b7cfdec60693
seorinn <80616701+seorinn@users.noreply.github.com>
2022-12-09 14:53:44 +0900

    Add files via upload

--- final_project/stage1.gd
+++ final_project/stage1.gd
@@ -11,6 +11,7 @@ func _process(delta):
 		isRotate = true
 		for i in range(18):
 			rotation_degrees += 5
+			$player.rotation_degrees-=5
 			yield(get_tree().create_timer(delta/2), "timeout")
 		yield(get_tree().create_timer(delta), "timeout")
 		isRotate = false
@@ -23,10 +24,13 @@ func _process(delta):
 		isRotate = true
 		for i in range(18):
 			rotation_degrees -= 5
+			$player.rotation_degrees+=5
 			yield(get_tree().create_timer(delta/2), "timeout")
 		yield(get_tree().create_timer(delta), "timeout")
 		isRotate = false
 		yield(get_tree().create_timer(1), "timeout")
 		enableKey = true
 
-
+	
+	if Input.is_action_just_pressed("esc") and enableKey:
+		get_tree().change_scene("res://Stages.tscn")
