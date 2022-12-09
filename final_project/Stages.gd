6aef5eaa474a93f765fb65f6150dc819ffc6910a
kangtp <115209262+kangtp@users.noreply.github.com>
2022-12-10 00:14:30 +0900

    Update Stages.gd

--- final_project/Stages.gd
+++ final_project/Stages.gd
@@ -9,5 +9,21 @@ func _on_QuitButton_pressed():
 	get_tree().change_scene("res://Main.tscn")
 
 
-func _on_Stage1Button_pressed():	
+func _on_Stage1Button_pressed():
+	get_tree().change_scene("res://stage0.tscn")
+
+
+func _on_Stage2Button_pressed():
+	get_tree().change_scene("res://stage0_5.tscn")
+
+
+func _on_Stage3Button_pressed():
 	get_tree().change_scene("res://stage1.tscn")
+
+
+func _on_Stage4Button_pressed():
+	get_tree().change_scene("res://stage3.tscn")
+
+
+func _on_Stage5Button_pressed():
+	get_tree().change_scene("res://stage2.tscn")
