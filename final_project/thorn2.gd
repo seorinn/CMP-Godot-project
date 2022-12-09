8370690c273f9dafb076ea2435f8fe593fd01bf1
kangtp <115209262+kangtp@users.noreply.github.com>
2022-12-10 01:31:51 +0900

    Update thorn2.gd

--- final_project/thorn2.gd
+++ final_project/thorn2.gd
@@ -4,6 +4,10 @@ extends Area2D
 
 
 func _on_thorn_body_entered(body):
-	body.queue_free()
-	get_tree().change_scene("res://stage2.tscn")
+	if(GameData.clear == -1):
+		if(GameData.effectSound == 1):
+			KilledSound.play()
+		body.queue_free()
+		get_tree().change_scene("res://stage2.tscn")
+
 
