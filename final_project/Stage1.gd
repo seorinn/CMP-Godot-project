5d9cc0de064ae3ff2af23e97775421ba347a2b40
kangtp <115209262+kangtp@users.noreply.github.com>
2022-12-09 23:17:32 +0900

    Update Stage1.gd

--- final_project/Stage1.gd
+++ final_project/Stage1.gd
@@ -9,11 +9,11 @@ func _process(delta):
 		isRotate = true
 		for i in range(18):
 			rotation_degrees += 5
-			$player.rotation_degrees-=5
+			#$player.rotation_degrees-=5
 			yield(get_tree().create_timer(delta/2), "timeout")
 		yield(get_tree().create_timer(delta), "timeout")
 		isRotate = false
-		yield(get_tree().create_timer(1), "timeout")
+		yield(get_tree().create_timer(0.3), "timeout")
 		enableKey = true
 
 
@@ -22,11 +22,11 @@ func _process(delta):
 		isRotate = true
 		for i in range(18):
 			rotation_degrees -= 5
-			$player.rotation_degrees+=5
+			#$player.rotation_degrees+=5
 			yield(get_tree().create_timer(delta/2), "timeout")
 		yield(get_tree().create_timer(delta), "timeout")
 		isRotate = false
-		yield(get_tree().create_timer(1), "timeout")
+		yield(get_tree().create_timer(0.3), "timeout")
 		enableKey = true
 
 	
