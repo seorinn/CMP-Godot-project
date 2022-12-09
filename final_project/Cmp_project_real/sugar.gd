e72b0daa4fff2394e804f8200a70588d634ea9b0
kangtp <115209262+kangtp@users.noreply.github.com>
2022-12-09 23:01:18 +0900

    Delete final_project/Cmp_project_real directory

--- final_project/Cmp_project_real/sugar.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-
-func _on_sugar_body_entered(body):
-	get_tree().change_scene("res://clear.tscn")
-	
--- final_project/Cmp_project_real/thorn2.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-
-
-func _on_thorn_body_entered(body):
-	body.queue_free()
-	get_tree().change_scene("res://stage2.tscn")
-
