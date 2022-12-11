62f760b2d7e9ebdf2416cb9e3c46b5752ad3aab4
21070-LDW <115209064+21070-LDW@users.noreply.github.com>
2022-12-12 00:51:21 +0900

    Update beCandy.gd

--- final_project/beCandy.gd
+++ final_project/beCandy.gd
@@ -1,8 +1,11 @@
 extends Node2D
-
+onready var at = get_node("/root/Attempts")
 func _ready():
 	if(GameData.musicPlaying == 1):
 		$bgMusic.play(GameData.bgPosition)
 		
 func _on_QuitButton_pressed():
+	
+	at.records.push_back(at.attemps)
+	at.attemps = 1
 	get_tree().change_scene("res://Main.tscn")
