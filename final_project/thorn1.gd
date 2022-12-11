4f36ec124928cb400258bd702eac6890529eb134
21070-LDW <115209064+21070-LDW@users.noreply.github.com>
2022-12-12 00:54:50 +0900

    Add files via upload

--- final_project/thorn1.gd
+++ final_project/thorn1.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+onready var at = get_node("/root/Attempts")
 
 func _on_thorn_body_entered(body):
 	if(GameData.clear == -1):
 		if(GameData.effectSound == 1):
 			KilledSound.play()
 		body.queue_free()
-		get_tree().change_scene("res://stage3.tscn")
+		at.attemps +=1
+		get_tree().reload_current_scene()
