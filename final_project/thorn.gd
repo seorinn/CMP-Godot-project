57442ec38f50c84108f7a008a07dff4744666437
kangtp <115209262+kangtp@users.noreply.github.com>
2022-12-11 15:57:45 +0900

    Update thorn.gd

--- final_project/thorn.gd
+++ final_project/thorn.gd
@@ -5,6 +5,6 @@ func _on_thorn_body_entered(body):
 		if(GameData.effectSound == 1):
 			KilledSound.play()
 		body.queue_free()
-		get_tree().change_scene("res://stage3.tscn")
+		get_tree().change_scene("res://stage4.tscn")
 
 
