57302102118ef655ee64d2417368a150608dd080
songjy002 <113113802+songjy002@users.noreply.github.com>
2022-12-13 12:37:05 +0900

    Add files via upload

---
+++ sadBean.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	if(GameData.musicPlaying == 1):
+		$bgMusic.play(GameData.bgPosition)
+
+func _on_SkipButton_pressed():
+	if(GameData.effectSound == 1):
+		GlobalButton.play()
+	get_tree().change_scene("res://howToPlay.tscn")
