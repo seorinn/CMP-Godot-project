5e5ca468fadb5afbc5004d6c64f41f92c16b6ce5
kangtp <115209262+kangtp@users.noreply.github.com>
2022-12-13 13:13:39 +0900

    Update thorn.gd

--- final_project/thorn.gd
+++ final_project/thorn.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 onready var at = get_node("/root/Attempts")
 
+#player enter the thorn, make sound effect, respawn and attemps+1
 func _on_thorn_body_entered(body):
 	if(GameData.clear == -1):
 		if(GameData.effectSound == 1):
 			KilledSound.play()
-		#body.queue_free()
 		at.attemps +=1
 		get_tree().reload_current_scene()
