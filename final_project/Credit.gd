7869b53b6a2293f610cc6b1b4ddb71d50d1be93a
seorinn <80616701+seorinn@users.noreply.github.com>
2022-12-13 20:35:51 +0900

    Update Credit.gd

--- final_project/Credit.gd
+++ final_project/Credit.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-
+#play BGM only when musicPlaying is on
 func _ready():
 	if(GameData.musicPlaying == 1):
 		$bgMusic.play(GameData.bgPosition)
 
+#button to go main scene
+#effect sound is played only when effectSound is on
 func _on_QuitButton_pressed():
 	if(GameData.effectSound == 1):
 		GlobalButton.play()
