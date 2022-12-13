64df4246915b1569ab46ca26a63498d9f0be0548
seorinn <80616701+seorinn@users.noreply.github.com>
2022-12-13 20:37:47 +0900

    Update GameData.gd

--- final_project/GameData.gd
+++ final_project/GameData.gd
@@ -1,7 +1,10 @@
 extends Node
 
-
+#is used for continueing BGM
 var bgPosition = 0
+#check music on/off
 var musicPlaying = 1
+#check effect sound on/off
 var effectSound = 1
+#for the situation that the player touched candy and thorn at the same time
 var clear = -1
