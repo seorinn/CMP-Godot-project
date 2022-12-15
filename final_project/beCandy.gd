ab1c6f2dd69d788a4eb6a237c0c3767a7bc936f0
seorinn <80616701+seorinn@users.noreply.github.com>
2022-12-15 14:05:31 +0900

    Update beCandy.gd

--- final_project/beCandy.gd
+++ final_project/beCandy.gd
@@ -3,9 +3,12 @@ onready var at = get_node("/root/Attempts")
 func _ready():
 	if(GameData.musicPlaying == 1):
 		$bgMusic.play(GameData.bgPosition)
+	if(GameData.effectSound == 1):
+		YaySound.play()	
 		
 func _on_QuitButton_pressed():
-	
+	if(GameData.effectSound == 1):
+		GlobalButton.play()	
 	at.records.push_back(at.attemps) # input this score
 	at.attemps = 1
 	get_tree().change_scene("res://Main.tscn")
